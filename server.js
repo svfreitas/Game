2c4b86f8c1ad0d7fc03311253314e2ee8f1b8660
svfreitas <svfreitas@yahoo.com>
2020-03-31 18:00:33 -0300

    process.env.PORT || 3000

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ sockets.on('connection', (socket) => {
     })
 })
 
-server.listen(3000, () => {
+let port = process.env.PORT || 3000
+server.listen(port, () => {
     console.log(`> Server listening on port: 3000`)
 })
